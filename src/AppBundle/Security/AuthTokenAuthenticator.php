30dfd3d3e80b93338210729d20a692c4ece70aea
Guillaume Cartié <guillaume.cartie@gmail.com>
2017-07-10 21:10:18 +0200

    set session duration

--- src/AppBundle/Security/AuthTokenAuthenticator.php
+++ src/AppBundle/Security/AuthTokenAuthenticator.php
@@ -19,7 +19,8 @@ class AuthTokenAuthenticator implements SimplePreAuthenticatorInterface, Authent
     /**
      * Durée de validité du token en secondes, 12 heures
      */
-    const TOKEN_VALIDITY_DURATION = 12 * 3600;
+//    const TOKEN_VALIDITY_DURATION = 12 * 3600;
+    const TOKEN_VALIDITY_DURATION = 1 *3600;
 
     protected $httpUtils;
 
